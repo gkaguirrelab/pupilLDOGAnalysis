72de5022cabe33d76c6450228b946f66ba049314
ozenc <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2020-08-04 15:38:35 -0800

    Adding the first draft for the motion correction script

---
+++ code/motionCorrectPupilVideos.m
@@ -0,0 +1,59 @@
+function motionCorrectPupilVideos(grayVideo, glintFileName, goodGlintFrame, outputPath) 
+%
+% Using the glint location in a good frame as the reference this function 
+% registers all frames to that good frame. Only does a translation (3DOF). 
+%
+% Syntax:
+%  motionCorrectPupilVideos(grayVideo, glintFileName, goodGlintFrame, outputPath) 
+%
+% Description:
+%   Simple motion correction for video drifting. Does not perform a perfect 
+%  motion correction as it only uses 3 DOF. Helps with setting up a good
+%  frame mask.
+% 
+%
+% Inputs:
+%   grayVideo             - String. Path to the input gray video.
+%   glintFileName         - String. Path to the glint file.
+%   goodGlintFrame        - Number. A good frame where the glint is visible
+%                           and the pupil is in a good location. Used as 
+%                           the fixed image for the registration.
+%   outputPath            - String. Path to the output folder and filename.
+%
+% Outputs:
+%   none
+%
+
+% Load the video and the glint
+rawVideoGray = VideoReader(grayVideo);
+glintFile = load(glintFileName);
+
+% Get the glint location on the selected good frame
+row_val = glintFile.glintData.Y(goodGlintFrame);
+col_val = glintFile.glintData.X(goodGlintFrame);
+
+% Create the video object and open it
+vidfile = VideoWriter(outputPath,'MPEG-4');
+open(vidfile);
+
+%% Loop through each frame and calculate the coordinate difference 
+for ii = goodGlintFrame:rawVideoGray.NumFrames
+    frame = read(rawVideoGray, ii);
+    r = glintFile.glintData.Y(ii);
+    c = glintFile.glintData.X(ii);
+    if isnan(r)
+        row_val_diff = 0;
+        col_val_diff = 0;
+    else
+        row_val_diff = row_val - r ;
+        col_val_diff = col_val - c;        
+    end 
+    % Translation
+    frame = imtranslate(frame, [row_val_diff, col_val_diff]);
+    % Write the video and show it on the screen
+    writeVideo(vidfile, frame)
+    imshow(frame)
+end
+
+% Close the video
+close(vidfile);
